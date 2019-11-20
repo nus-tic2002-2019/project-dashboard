d9afc7c97f41bb95c9e7464f4fe5d925898002dd
Damith C. Rajapakse <damith@gmail.com>
2019-11-20 20:00:15 +0800

    run.sh: fix typo in --master

--- run.sh
+++ run.sh
@@ -7,12 +7,12 @@
 ### ./get-reposense.py --tag v1.6.1            # Gets a specific version
 ### ./get-reposense.py --release --overwrite   # Overwrite RepoSense.jar, if exists, with the latest release
 
-./get-reposense.py --release
+./get-reposense.py --master
 
 # Executes RepoSense
 # Do not change the default output folder name (reposense-report)
 ## Examples of other valid options; For more, please view the user guide
 ### java -jar RepoSense.jar --repos https://github.com/reposense/RepoSense.git
 
-java -jar RepoSense.jar --master --config ./configs --since 1/09/2019 --formats java fxml sh bat gradle txt --timezone UTC+08
+java -jar RepoSense.jar --config ./configs --since 1/09/2019 --formats java fxml sh bat gradle txt --timezone UTC+08
 
